d3387175add7a73d2e14ff0228dec745a5712293
saklani <50768838+saklani@users.noreply.github.com>
2024-01-29 22:50:27 +0530

    basic structure + tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import aws from "astro-sst";
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Liteseed',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [
-    {
-      label: 'Getting Started',
-      items: [
-      {
-        label: 'What is Liteseed Network?',
-        link: '/getting-started/what-is-liteseed-network'
-      }
-      ]
-    } 
-    ],
-	  customCss: [
-        // Path to your Tailwind base styles:
-        './src/tailwind.css',
-      ],
-  }), tailwind({  applyBaseStyles: false })]
+	integrations: [
+		starlight({
+			title: 'Liteseed Network',
+			social: {
+				github: 'https://github.com/liteseed/documentation',
+			},
+			sidebar: [
+				{
+					label: 'Getting Started',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'What is Liteseed Network?', link: '/getting-started/what-is-liteseed-network' },
+					],
+				},
+			],
+		}),
+		tailwind()
+	],
+	output: "static",
+	adapter: aws(),
 });
---
+++ src/content/i18n/index.md
@@ -0,0 +1,33 @@
+---
+title: Welcome to Liteseed Network
+description: The decentralized network of bundlers on AO.
+template: splash
+hero:
+  tagline: The decentralized network of bundlers on AO.
+  image:
+    file: ../../assets/houston.webp
+  actions:
+    - text: Get Started 
+      link: /getting-started/what-is-liteseed-network/
+      icon: right-arrow
+      variant: primary
+---
+
+import { Card, CardGrid } from '@astrojs/starlight/components';
+
+## Next steps
+
+<CardGrid stagger>
+	<Card title="Update content" icon="pencil">
+		Edit `src/content/docs/index.mdx` to see this page change.
+	</Card>
+	<Card title="Add new content" icon="add-document">
+		Add Markdown or MDX files to `src/content/docs` to create new pages.
+	</Card>
+	<Card title="Configure your site" icon="setting">
+		Edit your `sidebar` and other config in `astro.config.mjs`.
+	</Card>
+	<Card title="Read the docs" icon="open-book">
+		Learn more in [the Starlight Docs](https://starlight.astro.build/).
+	</Card>
+</CardGrid>
