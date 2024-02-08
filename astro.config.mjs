21c9506869090cae7c05156b307f28116ae50bd6
saklani <50768838+saklani@users.noreply.github.com>
2024-02-08 15:41:11 +0530

    fix: www => docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://liteseed.xyz",
+  site: "https://docs.liteseed.xyz",
   integrations: [
     starlight({
       logo: {
